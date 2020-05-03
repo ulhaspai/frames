96be9009f275f84230311e0cd5878ba4b1fd3cde
Ulhas Pai <upai@kayak.com>
2020-05-02 20:50:19 -0400

    Reason: updates to the client application
    Description:
    - updated colors in the application
    - updated auth service to be able to fetch the current auth0 user as an observable
    - copy clean up

--- client/app/src/app/dashboard/dashboard.component.ts
+++ client/app/src/app/dashboard/dashboard.component.ts
@@ -2,15 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from "../login/auth.service";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(public auth: AuthService) { }
+    constructor(public auth: AuthService) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
 
+    }
+
+    printUser(): void {
+        this.auth.getUser().subscribe(user => {
+            console.log("User: ", user);
+            console.log("User email: ", user.email);
+            console.log("User token: ", user.token);
+        });
+    }
 }
